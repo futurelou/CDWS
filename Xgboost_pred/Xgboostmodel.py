571879a5e76260e0bab4e36e2ebd5f12ba2f7eb9
Louie Constable <Lzc5535@psu.edu>
2023-03-21 18:57:03 -0400

    backto normal

--- Xgboost_pred/Xgboostmodel.py
+++ Xgboost_pred/Xgboostmodel.py
@@ -5,47 +5,48 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import GridSearchCV
 import xgboost as xgb
 from sklearn.metrics import accuracy_score, f1_score
-class Xgboostmodel:
-    df = pd.read_csv(r'https://raw.githubusercontent.com/CovidWastewaterDetectionSystem/CDWS/main/df.csv',index_col=0)
 
-    cats = df.select_dtypes(exclude=np.number).columns.tolist()
+df = pd.read_csv(r'https://raw.githubusercontent.com/CovidWastewaterDetectionSystem/CDWS/main/df.csv',index_col=0)
 
-    for col in cats:
-        df[col] = df[col].astype('category')
+cats = df.select_dtypes(exclude=np.number).columns.tolist()
 
-
-    #splitting the data
+for col in cats:
+    df[col] = df[col].astype('category')
 
 
-    X = df.drop(['current_status','state_fips_code','county_fips_code','res_state', 'res_county','death_yn','process','icu_yn'], axis=1)
-    y = df['current_status']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=66)
-
-    xgtrain = xgb.DMatrix(X_train, label= y_train,enable_categorical=True)
+#splitting the data
 
 
-    param = {'max_depth': 1,
-             'eta': 0.01,
-             'objective': 'binary:logistic',
-             'eval_metric': 'error',
-             'nthread': 4}
+X = df.drop(['current_status','state_fips_code','county_fips_code','res_state', 'res_county','death_yn','process','icu_yn'], axis=1)
+y = df['current_status']
 
-    model = xgb.train(param, xgtrain, 400)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=66)
+
+xgtrain = xgb.DMatrix(X_train, label= y_train,enable_categorical=True)
 
 
-    def predict(test):
+param = {'max_depth': 1,
+         'eta': 0.01,
+         'objective': 'binary:logistic',
+         'eval_metric': 'error',
+         'nthread': 4}
 
-        xgtest = xgb.DMatrix(test, enable_categorical=True)
+model = xgb.train(param, xgtrain, 400)
 
-        preds = model.predict(xgtest)
 
-        for i in range(len(preds)):
-            if i<0.5:
-                preds[i]=0
-            elif i>0.5:
-                preds[i]=1
+def predict(test):
 
-        return preds
-    #test
+    xgtest = xgb.DMatrix(test, enable_categorical=True)
 
+    preds = model.predict(xgtest)
+
+    for i in range(len(preds)):
+        if i<0.5:
+            preds[i]=0
+        elif i>0.5:
+            preds[i]=1
+
+    return preds
+#test
+
+print(predict(X_test))
