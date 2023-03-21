6436087aa93311a1a1df1c9b9675540e375e3794
Louie Constable <Lzc5535@psu.edu>
2023-03-21 17:17:13 -0400

    make prediction function

--- Xgboostmodel.py
+++ Xgboostmodel.py
@@ -17,13 +17,13 @@ for col in cats:
 #splitting the data
 
 
-X = df.drop(['current_status','state_fips_code','county_fips_code'], axis=1)
+X = df.drop(['current_status','state_fips_code','county_fips_code','res_state', 'res_county','death_yn','process','icu_yn'], axis=1)
 y = df['current_status']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=66)
 
 xgtrain = xgb.DMatrix(X_train, label= y_train,enable_categorical=True)
-xgtest = xgb.DMatrix(X_test ,enable_categorical=True)
+
 
 param = {'max_depth': 1,
          'eta': 0.01,
@@ -33,15 +33,21 @@ param = {'max_depth': 1,
 
 model = xgb.train(param, xgtrain, 400)
 
+print(X_test.columns)
 
-preds = model.predict(xgtest)
+def predict(test):
 
-for i in range(len(preds)):
-    if i<0.5:
-        preds[i]=0
-    elif i>0.5:
-        preds[i]=1
+    xgtest = xgb.DMatrix(test, enable_categorical=True)
+
+    preds = model.predict(xgtest)
+
+    for i in range(len(preds)):
+        if i<0.5:
+            preds[i]=0
+        elif i>0.5:
+            preds[i]=1
+
+    return preds
 #test
 
-accuracy = accuracy_score(y_test,preds)
-f1 = f1_score(y_test,preds)
+
